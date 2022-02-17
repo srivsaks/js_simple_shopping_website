b8cd36c4a7be051f7dbe32ffb8d4435f96850546
Sakshi Srivastava <srivsaks@amazon.com>
2022-02-17 23:11:40 +0530

    Add elements dynamically to the list using bubbling

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+//Delete item
 let groceryListUL=document.querySelector("#grocery-list ul");
 groceryListUL.addEventListener("click",handleDelete);
 
@@ -6,4 +7,24 @@ function handleDelete(e){
         let liParent=e.target.parentElement;
         liParent.remove();
     }
+}
+
+// Add item to grocery list
+let addItemButton=document.querySelector("#add-item");
+addItemButton.addEventListener("submit",addItem);
+
+function addItem(e){
+    e.preventDefault();
+    let text=addItemButton.querySelector("input").value
+    let newLiElement=document.createElement("li");
+    let newItem=document.createElement("span");
+    newItem.className="item";
+    newItem.textContent=text;
+    let newDeleteButton=document.createElement("span");
+    newDeleteButton.className="delete";
+    newDeleteButton.textContent="Delete";
+    newLiElement.appendChild(newItem);
+    newLiElement.appendChild(newDeleteButton)
+    groceryListUL.appendChild(newLiElement);
+    addItemButton.querySelector("input").value=""
 }
